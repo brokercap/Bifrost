b50b0abac598ade14cd82830866ef27ab9908dbe
zero <jc3wish@126.com>
2019-03-26 21:39:21 +0800

    kafka plugin

---
+++ plugin/kafka/www/kafka.js
@@ -0,0 +1,27 @@
+function doGetPluginParam(){
+	var result = {data:{},status:false,msg:"error"}
+    var data = {};
+
+	var Topic = $("#Kafka_Topic").val();
+	var Key = $("#Kafka_Key").val();
+	var BatchSize = $("#Kafka_BatchSize").val();
+	
+    if (Topic == ""){
+		result.msg = "Topic can't be empty"
+        return result;
+    }
+	
+    if (BatchSize != "" && BatchSize != null && isNaN(BatchSize)){
+		result.msg = "BatchSize must be int!"
+        return result;
+    }
+
+	data["Topic"] = Topic;
+	data["Key"] = Key;
+	data["BatchSize"] = parseInt(BatchSize);
+
+	result.data = data;
+	result.msg = "success";
+	result.status = true;
+    return result;
+}
