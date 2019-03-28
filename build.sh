00da16dfc16735762f3c1bd9309a311e2e17dd16
jc3wish <xiezuofu@xiezuofudeMacBook-Pro.local>
2019-03-28 22:12:08 +0800

    编译脚本更新

--- build.sh
+++ build.sh
@@ -1,6 +1,12 @@
 #/bin/bash
 cd `dirname $0`
 mode=$1
+
+if [ ! -n "$2" ] ;then
+tagDir=tags/$1
+else
+tagDir=tags/$2/$1
+fi
 case "$mode" in
     'windows')
         ;;
@@ -15,24 +21,35 @@ case "$mode" in
         ;;
 esac
 
-mkdir -p $1/manager
-mkdir -p $1/etc
+mkdir -p $tagDir/manager
+mkdir -p $tagDir/plugin
+mkdir -p $tagDir/etc
 
 
 CGO_ENABLED=0 GOOS=$mode GOARCH=amd64 go build ./Bifrost.go
 
 if [[ "$1" == "windows" ]];then
-    mv Bifrost.exe ./windows
-    cp -rf ./manager/template ./windows/manager/template
-    cp -rf ./manager/public ./windows/manager/public
-    cp -f ./etc/Bifrost.ini ./$1/etc
+    mv Bifrost.exe ./$tagDir
 else
-    mv ./Bifrost ./$1
-    cp -rf ./manager/template ./$1/manager/template
-    cp -rf ./manager/public ./$1/manager/public
-    cp -f ./etc/Bifrost.ini ./$1/etc
-    cp -f ./Bifrost-server ./$1
+    mv ./Bifrost ./$tagDir
+    cp -f ./Bifrost-server ./$tagDir
 fi
+
+cp -rf ./manager/template ./$tagDir/manager/template
+cp -rf ./manager/public ./$tagDir/manager/public
+cp -f ./etc/Bifrost.ini ./$tagDir/etc
+for element in `ls ./plugin`
+do
+    dir_or_file="./plugin/"$element
+    if [ -d $dir_or_file ]
+    then
+        if [ -d $dir_or_file/www ]
+        then
+            mkdir -p $tagDir/plugin/$element
+            cp -rf $dir_or_file/www $tagDir/plugin/$element/
+        fi
+    fi
+done
 
 echo "$1 build over"
 
