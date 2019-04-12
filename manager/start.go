e2548c65210dce103e6077127c608037b9619b99
zero <jc3wish@126.com>
2019-04-12 18:37:07 +0800

    新增administrator及monitor用户组 新增http Basic 验证

--- manager/start.go
+++ manager/start.go
@@ -89,11 +89,56 @@ func index_controller(w http.ResponseWriter,req *http.Request){
 	http.Redirect(w, req, "/db/list", http.StatusFound)
 }
 
+var writeRequestOp = []string{"/add","/del","/deal"}
+//判断是否为写操作
+func checkWriteRequest(uri string) bool {
+	for _,v := range writeRequestOp{
+		if strings.Contains(uri,v){
+			return true
+		}
+	}
+	return false
+}
+
 func controller_FirstCallback(w http.ResponseWriter,req *http.Request) bool {
+	if req.Header.Get("Authorization") != ""{
+		return basicAuthor(w,req)
+	}else{
+		return normalAuthor(w,req)
+	}
+}
+
+func basicAuthor(w http.ResponseWriter,req *http.Request) bool{
+	UserName,Password,ok := req.BasicAuth()
+	if !ok || UserName == ""{
+		w.Write(returnDataResult(false,"Author error",""))
+		return false
+	}
+	pwd := config.GetConfigVal("user",UserName)
+	if pwd == Password{
+		GroupName := config.GetConfigVal("groups",UserName)
+		if GroupName != "administrator" && checkWriteRequest(req.RequestURI){
+			w.Write(returnDataResult(false,"user group : [ "+GroupName+" ] no authority",""))
+			return false
+		}
+		return true
+	}else{
+		w.Write(returnDataResult(false,"Password error",""))
+	}
+	return false
+}
+
+func normalAuthor(w http.ResponseWriter,req *http.Request) bool{
 	var sessionID= sessionMgr.CheckCookieValid(w, req)
 
 	if sessionID != "" {
 		if _,ok:=sessionMgr.GetSessionVal(sessionID,"UserName");ok{
+			//非administrator用户 用户，没有写操作权限
+			Group,_ := sessionMgr.GetSessionVal(sessionID,"Group")
+			if Group.(string) != "administrator" && checkWriteRequest(req.RequestURI){
+				w.Write(returnDataResult(false,"user group : [ "+Group.(string)+" ] no authority",""))
+				return false
+			}
 			return true
 		}else{
 			goto toLogin
@@ -103,11 +148,11 @@ func controller_FirstCallback(w http.ResponseWriter,req *http.Request) bool {
 	}
 
 	toLogin:
-		if req.RequestURI != "/login" && req.RequestURI != "/dologin" && req.RequestURI != "/logout"{
-			http.Redirect(w, req, "/login", http.StatusFound)
-			return false
-		}
-		return true
+	if req.RequestURI != "/login" && req.RequestURI != "/dologin" && req.RequestURI != "/logout"{
+		http.Redirect(w, req, "/login", http.StatusFound)
+		return false
+	}
+	return true
 }
 
 func Start(IpAndPort string){
