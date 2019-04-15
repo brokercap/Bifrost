44a2f9fef9a4690be40aa619acfcf622067e415d
zero <jc3wish@126.com>
2019-04-15 19:59:35 +0800

    重启的时候根据 表设置同步的最后位点和最后进入队列的位点，及 DB最后解析的位点，算出一个重启之前所有表同步的成功处理的最小位点

--- server/recovery.go
+++ server/recovery.go
@@ -21,6 +21,7 @@ import (
 	"log"
 	"strings"
 	"fmt"
+	"strconv"
 )
 
 type dbSaveInfo struct {
@@ -89,6 +90,7 @@ func recoveryData(data map[string]dbSaveInfo){
 		}
 		var BinlogFileNum int = 0
 		var BinlogPosition uint32 = 0
+		var lastAllToServerNoraml bool = true
 		if len(dbInfo.TableMap) > 0 {
 			for tKey, tInfo := range dbInfo.TableMap {
 				i := strings.IndexAny(tKey, "-")
@@ -96,24 +98,66 @@ func recoveryData(data map[string]dbSaveInfo){
 				tableName := tKey[i+1:]
 				db.AddTable(schemaName, tableName, channelIDMap[tInfo.ChannelKey],tInfo.LastToServerID)
 				for _, toServer := range tInfo.ToServerList {
-					db.AddTableToServer(schemaName, tableName,
-						&ToServer{
-							ToServerID:		toServer.ToServerID,
-							MustBeSuccess:  toServer.MustBeSuccess,
-							FilterQuery:  	toServer.FilterQuery,
-							FilterUpdate:  	toServer.FilterUpdate,
-							ToServerKey:    toServer.ToServerKey,
-							PluginName:   	toServer.PluginName,
-							FieldList:      toServer.FieldList,
-							BinlogFileNum:  toServer.BinlogFileNum,
-							BinlogPosition: toServer.BinlogPosition,
-							PluginParam:    toServer.PluginParam,
-						})
+
 					toServerBinlogPosition,_ := getBinlogPosition(getToServerBinlogkey(db,toServer))
 					if toServerBinlogPosition != nil{
 						toServer.BinlogFileNum  = toServerBinlogPosition.BinlogFileNum
 						toServer.BinlogPosition = toServerBinlogPosition.BinlogPosition
 					}
+					toServerLastBinlogPosition,_ := getBinlogPosition(getToServerLastBinlogkey(db,toServer))
+					if toServerLastBinlogPosition != nil{
+						toServer.LastBinlogFileNum = toServerLastBinlogPosition.BinlogFileNum
+						toServer.LastBinlogPosition = toServerLastBinlogPosition.BinlogPosition
+					}
+
+					db.AddTableToServer(schemaName, tableName,
+						&ToServer{
+							ToServerID:			toServer.ToServerID,
+							MustBeSuccess:  	toServer.MustBeSuccess,
+							FilterQuery:  		toServer.FilterQuery,
+							FilterUpdate:  		toServer.FilterUpdate,
+							ToServerKey:    	toServer.ToServerKey,
+							PluginName:   		toServer.PluginName,
+							FieldList:      	toServer.FieldList,
+							BinlogFileNum:  	toServer.BinlogFileNum,
+							BinlogPosition: 	toServer.BinlogPosition,
+							PluginParam:    	toServer.PluginParam,
+							LastBinlogPosition:	toServer.LastBinlogPosition,
+							LastBinlogFileNum: 	toServer.LastBinlogFileNum,
+						})
+
+					//假如当前同步配置 最后输入的 位点 等于 最后成功的位点，说明当前这个 同步配置的位点是没有问题的
+					if toServer.BinlogFileNum == toServer.LastBinlogFileNum && toServer.BinlogPosition == toServer.LastBinlogPosition{
+						if lastAllToServerNoraml == false{
+							//假如有一个同步不太正常的情况下，取小值
+							//这里用判断 BinlogFileNum == 0 是因为 绝对不会出现，因为绝对 第一次循环 lastAllToServerNoraml == true
+							if BinlogFileNum > toServer.BinlogFileNum{
+								BinlogFileNum = toServer.BinlogFileNum
+								BinlogPosition = toServer.BinlogPosition
+							}else{
+								if BinlogFileNum == toServer.BinlogFileNum && BinlogPosition > toServer.BinlogPosition{
+									BinlogPosition = toServer.BinlogPosition
+								}
+							}
+						}else{
+							//假如所有表都还是正常同步的情况下，取大值
+							if BinlogFileNum == 0 || BinlogFileNum < toServer.BinlogFileNum{
+								BinlogFileNum = toServer.BinlogFileNum
+								BinlogPosition = toServer.BinlogPosition
+							}else{
+								if BinlogFileNum == toServer.BinlogFileNum && BinlogPosition < toServer.BinlogPosition{
+									BinlogPosition = toServer.BinlogPosition
+								}
+							}
+						}
+						continue
+					}
+					lastAllToServerNoraml = false
+
+					if toServer.LastBinlogFileNum == 0 && toServer.BinlogFileNum > 0{
+						saveBinlogPosition(getToServerLastBinlogkey(db,toServer),toServer.BinlogFileNum,toServer.BinlogPosition)
+					}
+
 					if BinlogFileNum == 0{
 						BinlogFileNum = toServer.BinlogFileNum
 						BinlogPosition = toServer.BinlogPosition
@@ -137,7 +181,36 @@ func recoveryData(data map[string]dbSaveInfo){
 		// 二进制文件格式是xxx.000001 ,后面数字是6位数，不够前面补0
 		index := strings.IndexAny(dbInfo.BinlogDumpFileName, ".")
 		binlogPrefix := dbInfo.BinlogDumpFileName[0:index]
+
+		// 拿镜像数据里的 位点和level中存储 db 位点对比，取更大的值
+		// 因为镜像数据只有配置更改了才会修改，但是 level中的数据是只要有数据同步 及 每5秒强制刷一次
+		LastDBBinlogFileNum,_ := strconv.Atoi(dbInfo.BinlogDumpFileName[index+1:])
+		DBBinlogKey := getDBBinlogkey(db)
+		DBLastBinlogPosition,_ := getBinlogPosition(DBBinlogKey)
+		if DBLastBinlogPosition != nil{
+			if LastDBBinlogFileNum < DBLastBinlogPosition.BinlogFileNum{
+				LastDBBinlogFileNum = DBLastBinlogPosition.BinlogFileNum
+				db.binlogDumpPosition = DBLastBinlogPosition.BinlogPosition
+			}else if LastDBBinlogFileNum == DBLastBinlogPosition.BinlogFileNum{
+				if db.binlogDumpPosition < DBLastBinlogPosition.BinlogPosition{
+					db.binlogDumpPosition = DBLastBinlogPosition.BinlogPosition
+				}
+			}
+		}
+		db.binlogDumpFileName = binlogPrefix+"."+fmt.Sprintf("%06d",LastDBBinlogFileNum)
+
+
+		//假如所有表数据同步都是正常的，则取 db 里的位点配置，否则取 同步表里最小位点
+		if lastAllToServerNoraml == false{
+			db.binlogDumpFileName = binlogPrefix+"."+fmt.Sprintf("%06d",BinlogFileNum)
+			db.binlogDumpPosition = BinlogPosition
+		}else{
+			db.binlogDumpFileName = dbInfo.BinlogDumpFileName
+			db.binlogDumpPosition = dbInfo.BinlogDumpPosition
+		}
+
 		//找到最小的位点位置进行更新到 db 配置中去，进行slave连接
+		/*
 		if BinlogFileNum > 0 && BinlogPosition > 0{
 			db.binlogDumpFileName = binlogPrefix+"."+fmt.Sprintf("%06d",BinlogFileNum)
 			db.binlogDumpPosition = BinlogPosition
@@ -150,6 +223,7 @@ func recoveryData(data map[string]dbSaveInfo){
 				db.binlogDumpPosition = DBBinlogPosition.BinlogPosition
 			}
 		}
+		*/
 
 		if dbInfo.ConnStatus == "closing"{
 			dbInfo.ConnStatus = "close"
