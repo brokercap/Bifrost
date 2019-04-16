6814b26170f580a495b14f5d57aaadb7c7e763c1
jc3wish <xiezuofu@xiezuofudeMacBook-Pro.local>
2019-04-16 23:05:57 +0800

    流量显示页及js封装

--- manager/public/js/flow.js
+++ manager/public/js/flow.js
@@ -1,84 +1,95 @@
-class FlowClass {
-    CountType = "条";
-    ByteSizeType = "b";
-    dbName  = "";
-    schema = "";
-    tableName = "";
-    ChanneId = "";
-    type = "tenminute";
-    CanvasId = "";
-    ChartType = "line";
-    DisplayFormat = "increment"; //increment,full;
+var FlowClass  = {
+    CountType: "条",
+    ByteSizeType: "b",
+    dbName: "",
+    schema: "",
+    tableName: "",
+    ChanneId: "",
+    AgetLength: "tenminute",
+    CanvasId: "",
+    ChartType: "line",
+    DisplayFormat: "increment",//increment,full;
 
-    constructor(Type,CanvasId,DisplayFormat) {
-        this.type = Type;
-        this.CanvasId = CanvasId;
-        this.DisplayFormat = DisplayFormat;
-    }
-    setDbName=function(dbName){
+    CountSum:0,
+    ByteSizeSum:0,
+
+    getCountSum:function () {
+        return this.CountSum;
+    },
+
+    getByteSizeSum:function () {
+        return this.ByteSizeSum;
+    },
+
+    setDbName: function (dbName) {
         this.dbName = dbName;
-    }
-    setSchema=function(schema){
+    },
+    setSchema: function (schema) {
         this.schema = schema;
-    }
+    },
 
-    setTableName=function(tableName){
+    setChanneId:function (ChanneId) {
+        this.ChanneId = ChanneId;
+    },
+    setTableName: function (tableName) {
         this.tableName = tableName;
-    }
+    },
 
-    setType=function(type){
-        this.type = type;
-    }
-    setCanvasId=function(CanvasId){
+    setAgetLength: function (AgetLength) {
+        this.AgetLength = AgetLength;
+    },
+    setCanvasId: function (CanvasId) {
         this.CanvasId = CanvasId;
-    }
+    },
 
-    setDisplayFormat=function(DisplayFormat){
+    setDisplayFormat: function (DisplayFormat) {
         this.DisplayFormat = DisplayFormat;
-    }
+    },
 
-    add0 = function(m){return m<10?'0'+m:m }
+    add0: function (m) {
+        return m < 10 ? '0' + m : m
+    },
 
-    TimeFormat = function(timeUnix)
-    {
-        var time = new Date(parseInt(timeUnix)*1000);
+    TimeFormat: function (timeUnix) {
+        var time = new Date(parseInt(timeUnix) * 1000);
         var y = time.getFullYear();
         var m = time.getMonth();
         var d = time.getDate();
         var h = time.getHours();
         var mm = time.getMinutes();
         var s = time.getSeconds();
-        return y+'-'+this.add0(m)+'-'+this.add0(d)+' '+this.add0(h)+':'+this.add0(mm)+':'+this.add0(s);
-    }
+        return this.add0(h) + ':' + this.add0(mm) + ':' + this.add0(s);
+        //return y + '-' + this.add0(m) + '-' + this.add0(d) + ' ' + this.add0(h) + ':' + this.add0(mm) + ':' + this.add0(s);
+    },
 
-    rewrite_data = function (d){
-        if (d.length == 0){
+    rewrite_data: function (d) {
+        if (d.length == 0) {
             return false
         }
         var ChartData = {};
         ChartData.options = {};
-        ChartData.labels=[];
-        ChartData.datasets=[];
+        ChartData.labels = [];
+        ChartData.datasets = [];
 
-        var ByteSizeData={};
-        ByteSizeData.data=[];
-        ByteSizeData.fillColor= "#1ab394";
+        var ByteSizeData = {};
+        ByteSizeData.data = [];
+        ByteSizeData.fillColor = "#1ab394";
         ByteSizeData.strokeColor = "#1ab394";
-        ByteSizeData.highlightFill="#1ab394";
-        ByteSizeData.highlightStroke= "#1ab394";
+        ByteSizeData.highlightFill = "#1ab394";
+        ByteSizeData.highlightStroke = "#1ab394";
 
-        ByteSizeData.borderColor ="#1ab394";
-        ByteSizeData.label = "ByteSize("+ByteSizeType+")";
+        ByteSizeData.borderColor = "#1ab394";
+        ByteSizeData.label = "ByteSize(" + ByteSizeType + ")";
 
         var CountData = {};
-        CountData.data=[];
-        CountData.fillColor= "#5CACEE";
+        CountData.data = [];
+        CountData.fillColor = "#5CACEE";
         CountData.strokeColor = "#5CACEE";
-        CountData.highlightFill="#5CACEE";
-        CountData.highlightStroke= "#5CACEE";
+        CountData.highlightFill = "#5CACEE";
+        CountData.highlightStroke = "#5CACEE";
 
-        CountData.borderColor ="#5CACEE";
-        CountData.label = "Count("+CountType+")";
+        CountData.borderColor = "#5CACEE";
+        CountData.label = "Count(" + CountType + ")";
         for (i in d) {
             ChartData.labels.push(d[i].time);
             ByteSizeData.data.push(d[i].ByteSize);
@@ -86,31 +97,31 @@ class FlowClass {
         }
         ChartData.datasets.push(ByteSizeData);
         ChartData.datasets.push(CountData);
-        if($("#"+CanvasId).length > 0) {
-            var ctx = document.getElementById(CanvasId).getContext("2d");
-            var chart = new Chart(ctx, {type:this.ChartType,data:ChartData});
+        if ($("#" + this.CanvasId).length > 0) {
+            var ctx = document.getElementById(this.CanvasId).getContext("2d");
+            var chart = new Chart(ctx, {type: this.ChartType, data: ChartData});
         }
-    }
+    },
 
-    incrementData = function(d){
+    incrementData: function (d) {
         var data = [];
         CountType = "条";
         ByteSizeType = "b";
         var Count = -1;
         var ByteSize = -1;
         for (s in d) {
-            if (d[s].Time > 0){
-                if (Count == -1){
+            if (d[s].Time > 0) {
+                if (Count == -1) {
                     Count = d[s].Count;
                     ByteSize = d[s].ByteSize;
                     continue;
                 }
-                var tSize = d[s].ByteSize-ByteSize;
-                if( tSize < 0){
+                var tSize = d[s].ByteSize - ByteSize;
+                if (tSize < 0) {
                     tSize = 0;
                 }
-                var tCount = d[s].Count-Count;
-                if (tCount < 0){
+                var tCount = d[s].Count - Count;
+                if (tCount < 0) {
                     tCount = 0;
                 }
                 data.push({
@@ -120,51 +131,53 @@ class FlowClass {
                 });
                 Count = d[s].Count;
                 ByteSize = d[s].ByteSize;
+                this.ByteSizeSum += ByteSize;
+                this.CountSum += Count;
             }
         }
         return data;
-    }
+    },
 
-    fullData = function(d){
+    fullData: function (d) {
         var data = [];
         CountType = "条"
         ByteSizeType = "b"
-        if (d[0].Count > 100000){
+        if (d[0].Count > 100000) {
             CountType = "k"
         }
-        if (d[0].ByteSize >= 1024000 ){
+        if (d[0].ByteSize >= 1024000) {
             ByteSizeType = "kb"
         }
 
-        if (d[0].ByteSize >= 1024000000 ){
+        if (d[0].ByteSize >= 1024000000) {
             ByteSizeType = "MB"
         }
 
-        if (d[0].ByteSize >= 1024000000000 ){
+        if (d[0].ByteSize >= 1024000000000) {
             ByteSizeType = "GB"
         }
 
         for (s in d) {
-            if (d[s].Time != ""){
+            if (d[s].Time != "") {
                 var Count = 0
-                if (CountType == "k"){
-                    Count =(d[s].Count/1000).toFixed(2)
-                }else{
+                if (CountType == "k") {
+                    Count = (d[s].Count / 1000).toFixed(2)
+                } else {
                     Count = d[s].Count
                 }
                 var ByteSize = 0
-                switch (ByteSizeType){
+                switch (ByteSizeType) {
                     case "b":
                         ByteSize = d[s].ByteSize;
                         break
                     case "kb":
-                        ByteSize =(d[s].ByteSize/1024).toFixed(2)
+                        ByteSize = (d[s].ByteSize / 1024).toFixed(2)
                         break
                     case "MB":
-                        ByteSize =(d[s].ByteSize/1024000).toFixed(2)
+                        ByteSize = (d[s].ByteSize / 1024000).toFixed(2)
                         break
                     case "GB":
-                        ByteSize =(d[s].ByteSize/1024000000).toFixed(2)
+                        ByteSize = (d[s].ByteSize / 1024000000).toFixed(2)
                         break
                 }
 
@@ -173,12 +186,18 @@ class FlowClass {
                     Count: Count,
                     ByteSize: ByteSize,
                 });
+
+                this.ByteSizeSum = ByteSize;
+                this.CountSum = Count;
             }
         }
         return data;
-    }
+    },
 
-    getFlowData() {
+    getFlowData: function () {
+        var obj = this;
+        this.ByteSizeSum = 0;
+        this.CountSum = 0;
         $.post(
             "/flow/get",
             {
@@ -186,19 +205,19 @@ class FlowClass {
                 schema: this.schema,
                 table_name: this.tableName,
                 channelid: this.ChanneId,
-                type: this.type,
+                type: this.AgetLength,
             },
             function (d, status) {
                 if (status != "success") {
                     return false;
                 }
 
-                if (this.DisplayFormat == "full"){
-                    this.rewrite_data(this.fullData(d));
-                }else{
-                    this.rewrite_data(this.incrementData(d));
+                if (this.DisplayFormat == "full") {
+                    obj.rewrite_data(obj.fullData(d));
+                } else {
+                    obj.rewrite_data(obj.incrementData(d));
                 }
 
             }, 'json');
-    }
+    },
 }
