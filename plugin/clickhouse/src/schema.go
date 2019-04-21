7a4205f5f175e6d213debfcc30a5d9187c794f33
jc3wish <xiezuofu@xiezuofudeMacBook-Pro.local>
2019-04-21 23:18:55 +0800

    plugin ck apha

--- plugin/clickhouse/src/schema.go
+++ plugin/clickhouse/src/schema.go
@@ -3,7 +3,25 @@ package src
 import (
 	"database/sql"
 	_ "github.com/kshvakov/clickhouse"
+	"os/exec"
+	"os"
+	"path/filepath"
+	"github.com/jc3wish/Bifrost/manager/xgo"
+	"net/http"
 )
+
+var execDir string
+
+func init()  {
+	xgo.AddRoute("/bifrost/clickhouse/tableinfo",getClickHouseTableFields)
+	execPath, _ := exec.LookPath(os.Args[0])
+	execDir = filepath.Dir(execPath)+"/"
+}
+
+func getClickHouseTableFields(w http.ResponseWriter,req *http.Request)  {
+
+}
+
 func newClickHouseDBConn()  {
 
 }
@@ -19,5 +37,23 @@ func(This *clickhouseDB) Open() bool{
 	return true
 }
 
-func (This *clickhouseDB) getTableInfo(TableName string)  {
+func (This *clickhouseDB) getTableFields(TableName string) (data map[string]string) {
+	rows,err := This.conn.Query("DESC TABLE "+TableName)
+	if err != nil {
+		This.err = err
+		return
+	}
+	defer rows.Close()
+	for rows.Next() {
+		var (
+			Name               string
+			Type           	string
+		)
+		if err := rows.Scan(&Name, &Type); err != nil {
+			This.err = err
+			return
+		}
+		data[Name] = Type
+	}
+	return
 }
