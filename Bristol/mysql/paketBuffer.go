fc7d452b586b56627463e0248d322d676134b981
zero <jc3wish@126.com>
2019-07-11 19:32:46 +0800

    解决decimal类型解析错误的bug

--- Bristol/mysql/paketBuffer.go
+++ Bristol/mysql/paketBuffer.go
@@ -2,6 +2,7 @@ package mysql
 
 import (
 	"bytes"
+	"encoding/binary"
 )
 
 func init() {}
@@ -12,26 +13,34 @@ type paket struct {
 }
 
 func (This *paket) read(size int) []byte {
-	if len(This.buydata) == size {
-		data := This.buydata
+	if len(This.buydata) == 0{
+		return This.buf.Next(size)
+	}
+	l := size - len(This.buydata)
+	if l == 0{
+		data := This.buydata[0:size]
 		This.buydata = make([]byte, 0)
 		return data
 	}
-	l := size - len(This.buydata)
-	if l > 0 {
-		This.advance(l)
-		return This.read(size)
-	} else {
+	if l < 0 {
 		data := This.buydata[0:size]
-		This.buydata = This.buydata[0:size]
+		This.buydata = This.buydata[size:]
+		return data
+	} else {
+		data := This.buydata[0:]
+		This.buydata = make([]byte, 0)
+		tmp := This.buf.Next(l)
+		for _, b := range tmp {
+			data = append(data,b)
+		}
 		return data
 	}
 }
 
 func (This *paket) readByte() byte {
-	if len(This.buydata) >= 1 {
+	if len(This.buydata) > 0 {
 		b := This.buydata[0:1]
-		This.buydata = This.buydata[0:1]
+		This.buydata = This.buydata[1:]
 		return b[0]
 	} else {
 		b, _ := This.buf.ReadByte()
@@ -39,13 +48,69 @@ func (This *paket) readByte() byte {
 	}
 }
 
-func (This *paket) advance(size int) {
-	data := This.buf.Next(size)
-	This.unread(data)
-}
-
 func (This *paket) unread(data []byte) {
 	for _, b := range data {
 		This.buydata = append(This.buydata, b)
 	}
+}
+
+
+func (This *paket) intRead(size int) int{
+
+	var x int
+	switch size {
+	case 1:
+		var a int8
+		b_buf  :=  bytes.NewBuffer(This.read(size))
+		binary.Read(b_buf, binary.BigEndian, &a)
+		x = int(a)
+		break
+	case 2:
+		buf := This.read(size)
+		var y int16
+		b_buf  :=  bytes.NewBuffer(buf)
+		binary.Read(b_buf, binary.BigEndian, &y)
+		x = int(y)
+		break
+	case 3:
+		var a,b,c int8
+		b_buf  :=  bytes.NewBuffer(This.read(1))
+		binary.Read(b_buf, binary.BigEndian, &a)
+		b_buf  =  bytes.NewBuffer(This.read(1))
+		binary.Read(b_buf, binary.BigEndian, &b)
+		b_buf  =  bytes.NewBuffer(This.read(1))
+		binary.Read(b_buf, binary.BigEndian, &c)
+
+		res := (int(a) << 16) | (int(b) << 8) | int(c)
+		if res >= 0x800000{
+			res -= 0x1000000
+		}
+		x = res
+		break
+	case 4:
+		buf := This.read(size)
+		var y int32
+		b_buf  :=  bytes.NewBuffer(buf)
+		binary.Read(b_buf, binary.BigEndian, &y)
+		x = int(y)
+		break
+	case 5:
+		var a int32
+		var b int8
+		b_buf  :=  bytes.NewBuffer(This.read(4))
+		binary.Read(b_buf, binary.BigEndian, &a)
+		b_buf  =  bytes.NewBuffer(This.read(1))
+		binary.Read(b_buf, binary.BigEndian, &b)
+		x = int(b) + (int(a) << 8)
+		break
+	case 8:
+		buf := This.read(size)
+		var y int64
+		b_buf  :=  bytes.NewBuffer(buf)
+		binary.Read(b_buf, binary.BigEndian, &y)
+		x = int(y)
+		break
+	}
+
+	return x
 }
