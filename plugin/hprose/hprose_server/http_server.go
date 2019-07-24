4e9167a363be628088b2646b27df79014f1115c2
zero <jc3wish@126.com>
2019-07-24 18:40:02 +0800

    hprose插件server案例

--- plugin/hprose/hprose_server/http_server.go
+++ plugin/hprose/hprose_server/http_server.go
@@ -1,17 +1,20 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/hprose/hprose-golang/rpc"
-	"github.com/brokercap/Bifrost/hprose_server/serverdo"
+	"github.com/brokercap/Bifrost/plugin/hprose/hprose_server/serverdo"
 )
 
+
 func main() {
-	service := rpc.NewTCPServer("tcp4://0.0.0.0:4321/")
+	service := rpc.NewHTTPService()
 	service.Debug = true
 	service.AddFunction("Insert", serverdo.Insert)
 	service.AddFunction("Update", serverdo.Update)
 	service.AddFunction("Delete", serverdo.Delete)
-	service.AddFunction("Query", serverdo.Query)
+	service.AddFunction("ToList", serverdo.Query)
 	service.AddFunction("Check", serverdo.Check)
-	service.Start()
+	http.ListenAndServe(":8881", service)
 }
