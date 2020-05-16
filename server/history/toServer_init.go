bd6c578ef9995142281c2b19bf6a9f1e585005c2
jc3wish <jc3wish@126.com>
2020-05-16 17:04:28 +0800

    全量任务判断是否同步完毕等优化

--- server/history/toServer_init.go
+++ server/history/toServer_init.go
@@ -2,7 +2,7 @@ package history
 
 import (
 	"github.com/brokercap/Bifrost/server"
-	"log"
+	"sync"
 )
 
 func (This *History) InitToServer()  {
@@ -31,7 +31,6 @@ func (This *History) InitToServer()  {
 					Error		  		:"",
 					ErrorWaitDeal 		:0,
 					ErrorWaitData 		:nil,
-					PluginParamObj 		:nil,
 					LastBinlogFileNum   :0,                       // 由 channel 提交到 ToServerChan 的最后一个位点
 					LastBinlogPosition  :0,                   	 // 假如 BinlogFileNum == LastBinlogFileNum && BinlogPosition == LastBinlogPosition 则说明这个位点是没有问题的
 					LastBinlogKey 		:nil,         			 // 将数据保存到 level 的key
@@ -46,15 +45,19 @@ func (This *History) InitToServer()  {
 	}
 }
 
-func (This *History) AsyncWaitToServerOver()  {
+func (This *History) SyncWaitToServerOver(n int)  {
 	This.Lock()
 	defer This.Unlock()
-	if This.toServerTheadCountChan != nil{
+	if This.ToServerTheadGroup != nil {
+		This.ToServerTheadGroup.Add(n)
 		return
 	}
-	This.toServerTheadCountChan = make(chan int16,len(This.ToServerIDList))
+	This.ToServerTheadGroup = &sync.WaitGroup{}
+	This.ToServerTheadGroup.Add(n)
 	go func() {
 		defer func() {
+			This.Lock()
+			defer This.Unlock()
 			switch This.Status {
 			case HISTORY_STATUS_KILLED, HISTORY_STATUS_HALFWAY:
 				break
@@ -62,22 +65,7 @@ func (This *History) AsyncWaitToServerOver()  {
 				This.Status = HISTORY_STATUS_OVER
 				break
 			}
-			if This.ToServerTheadCount > 0 {
-				log.Println("history", This.DbName, This.SchemaName, This.TableName, " over, but sync threadNum:", This.ToServerTheadCount, " != 0")
-			}
-			This.toServerTheadCountChan = nil
 		}()
-		var i int16
-		for {
-			i = <- This.toServerTheadCountChan
-			if i == 0 {
-				return
-			}
-			This.ToServerTheadCount += i
-			//log.Println("AsyncWaitToServerOver i:",i," This.ToServerTheadCount:",This.ToServerTheadCount)
-			if This.ToServerTheadCount <= 0 {
-				return
-			}
-		}
+		This.ToServerTheadGroup.Wait()
 	}()
 }
