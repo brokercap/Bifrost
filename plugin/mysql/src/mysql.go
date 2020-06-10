620faa1f138c21c432f4ac67ec185e813cbec565
jc3wish <jc3wish@126.com>
2020-06-10 21:35:06 +0800

    修复mysql,ck 满批次丢失一条数据的bug

--- plugin/mysql/src/mysql.go
+++ plugin/mysql/src/mysql.go
@@ -13,8 +13,8 @@ import (
 )
 
 
-const VERSION  = "v1.2.1"
-const BIFROST_VERION = "v1.2.1"
+const VERSION  = "v1.2.2"
+const BIFROST_VERION = "v1.2.2"
 
 type dataTableStruct struct {
 	MetaMap			map[string]string //字段类型
@@ -374,7 +374,7 @@ func (This *Conn) Commit() (b *pluginDriver.PluginBinlog,e error) {
 	if len(This.p.Data.Data) <= int(This.p.BatchSize){
 		This.p.Data.Data = make([]*pluginDriver.PluginDataType,0)
 	}else{
-		This.p.Data.Data = This.p.Data.Data[n+1:]
+		This.p.Data.Data = This.p.Data.Data[n:]
 	}
 
 	return &pluginDriver.PluginBinlog{list[n-1].BinlogFileNum,list[n-1].BinlogPosition}, nil
