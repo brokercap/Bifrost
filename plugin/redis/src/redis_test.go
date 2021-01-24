929f488bb9a3a744855401350594fcd2171f4229
jc3wish <jc3wish@126.com>
2021-01-24 21:11:41 +0800

    fix redis uri pwd contain @, connect error bug

--- plugin/redis/src/redis_test.go
+++ plugin/redis/src/redis_test.go
@@ -282,3 +282,21 @@ func TestSyncLikeProductForSpeed(t *testing.T)  {
 	}
 
 }
+
+func TestGetUriParam(t *testing.T) {
+	url := "pwd@123@tcp(127.0.0.1:6379,127.0.0.1:6380)/16"
+	pwd,network,uri,database := MyPlugin.GetUriParam(url)
+
+	t.Log("pwd:",pwd)
+	t.Log("network:",network)
+	t.Log("uri:",uri)
+	t.Log("database:",database)
+
+	url = "127.0.0.1:6379"
+	pwd,network,uri,database = MyPlugin.GetUriParam(url)
+
+	t.Log("pwd:",pwd)
+	t.Log("network:",network)
+	t.Log("uri:",uri)
+	t.Log("database:",database)
+}
