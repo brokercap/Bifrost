511e05362cdc24553f607710399499809e60fb95
zy <zhangyuan@vchangyi.com>
2021-08-09 16:05:32 +0800

    增加clickhouse 本地表、分布式表，视图等。支持集群模式分布式写入 支持在线DDL同步。包含：增加字段，删除字段，修改字段类型，修改字段名称，修改表名称等。 同步界面增加集群，单机模式可选 ddl同步程度可选 兼容原单机模式 同时增加单机模式下的 同步DDL功能 由于涉及到final 效率问题，推荐ck版本为 v20.5.2.7-stable以上（包含）。

--- plugin/clickhouse/src/rename_sql_test.go
+++ plugin/clickhouse/src/rename_sql_test.go
@@ -1,22 +1,22 @@
 package src
 
 import (
-	"testing"
 	"strings"
+	"testing"
 )
 
 func TestReNameSQL_Transfer2CkSQL(t *testing.T) {
 	ckObj := &Conn{
-		p:&PluginParam{
-			CkSchema:"",
+		p: &PluginParam{
+			CkSchema: "",
 		},
 	}
 
 	sql := "rename table mytest22 to mytest;"
 	Query := ReplaceBr(sql)
 	Query = ReplaceTwoReplace(Query)
-	Query = strings.Trim(strings.Trim(strings.Trim(Query," "),";")," ")
-	c := NewReNameSQL("bifrost_test",Query,ckObj)
-	_,_,destAlterSql := c.Transfer2CkSQL()
+	Query = strings.Trim(strings.Trim(strings.Trim(Query, " "), ";"), " ")
+	c := NewReNameSQL("bifrost_test", Query, ckObj)
+	_, _, _, _, destAlterSql := c.Transfer2CkSQL(ckObj)
 	t.Log(destAlterSql)
 }
