d89817c2af55d0a295dd196e37e3f53da7f3561e
jc3wish <jc3wish@126.com>
2022-10-16 19:23:28 +0800

    修复input 多源之后mysql 源同步数据不了的bug

--- input/mysql/callback.go
+++ input/mysql/callback.go
@@ -50,19 +50,21 @@ func (c *MysqlInput) MySQLCallback(data *mysql.EventReslut) {
 	intString := data.BinlogFileName[i+1:]
 	BinlogFileNum, _ := strconv.Atoi(intString)
 	data0 := &pluginDriver.PluginDataType{
-		EventSize:      data.Header.EventSize,
-		Timestamp:      data.Header.Timestamp,
-		EventType:      evenTypeName(data.Header.EventType),
-		SchemaName:     data.SchemaName,
-		TableName:      data.TableName,
-		Rows:           data.Rows,
-		BinlogFileNum:  BinlogFileNum,
-		BinlogPosition: data.Header.LogPos,
-		Query:          data.Query,
-		Gtid:           data.Gtid,
-		Pri:            data.Pri,
-		ColumnMapping:  data.ColumnMapping,
-		EventID:        data.EventID,
+		EventSize:       data.Header.EventSize,
+		Timestamp:       data.Header.Timestamp,
+		EventType:       evenTypeName(data.Header.EventType),
+		SchemaName:      data.SchemaName,
+		TableName:       data.TableName,
+		AliasSchemaName: data.SchemaName,
+		AliasTableName:  data.TableName,
+		Rows:            data.Rows,
+		BinlogFileNum:   BinlogFileNum,
+		BinlogPosition:  data.Header.LogPos,
+		Query:           data.Query,
+		Gtid:            data.Gtid,
+		Pri:             data.Pri,
+		ColumnMapping:   data.ColumnMapping,
+		EventID:         data.EventID,
 	}
 	c.callback(data0)
 }
