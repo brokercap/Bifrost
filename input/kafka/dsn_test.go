89c75bc2e80aba950da5d799629dfcdd56a4222e
jc3wish <jc3wish@126.com>
2022-10-20 22:06:37 +0800

    修复kafka input url param 中带 / 造成解析不了的BUG

--- input/kafka/dsn_test.go
+++ input/kafka/dsn_test.go
@@ -25,8 +25,14 @@ func TestParseDSN(t *testing.T) {
 
 	url = "127.0.0.1:9092,10.10.10.10?from.beginning=false"
 	p = ParseDSN(url)
-	if len(p) != 1 {
-		t.Fatalf("len(p) != 1")
+	if len(p) != 2 {
+		t.Fatalf("len(p) != 2")
+	}
+
+	url = "127.0.0.1:9092,192.168.1.10/topic1,topic2?from.beginning=false&ca=/root/a.pem&cert=/root/cert.pem"
+	p = ParseDSN(url)
+	if len(p) != 5 {
+		t.Fatalf("len(p) != 5 %+v", p)
 	}
 
 	url = "127.0.0.1:9092,192.168.1.10/topic1,topic2?from.beginning=false"
@@ -38,7 +44,7 @@ func TestParseDSN(t *testing.T) {
 		t.Fatalf("addr(%s) != %s", p["addr"], "127.0.0.1:9092,192.168.1.10")
 	}
 	if p["topics"] != "topic1,topic2" {
-		t.Fatalf("topics != %s", "topic1,topic2")
+		t.Fatalf("topics(%s) != %s", p["topics"], "topic1,topic2")
 	}
 	if p["from.beginning"] != "false" {
 		t.Fatalf("from.beginning != %s", "false")
