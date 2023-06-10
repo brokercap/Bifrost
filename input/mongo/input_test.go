b74cfbd56f5765ae8c78d5333c779c5b8117d53f
jc3wish <jc3wish@126.com>
2023-06-10 10:11:36 +0800

    input mongo 单测新增部分函数

--- input/mongo/input_test.go
+++ input/mongo/input_test.go
@@ -1,13 +1,21 @@
 package mongo
 
 import (
+	"context"
+	"fmt"
+	"github.com/agiledragon/gomonkey"
 	inputDriver "github.com/brokercap/Bifrost/input/driver"
 	outputDriver "github.com/brokercap/Bifrost/plugin/driver"
-	"log"
+	"github.com/rwynn/gtm/v2"
+	. "github.com/smartystreets/goconvey/convey"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"reflect"
 	"testing"
 	"time"
 )
 
+/*
 func monitorDump(reslut chan *inputDriver.PluginStatus, plugin inputDriver.Driver, t *testing.T) (r bool) {
 	timer := time.NewTimer(3 * time.Second)
 	defer timer.Stop()
@@ -27,7 +35,9 @@ func monitorDump(reslut chan *inputDriver.PluginStatus, plugin inputDriver.Drive
 		}
 	}
 }
+*/
 
+/*
 func callback(data *outputDriver.PluginDataType) {
 	log.Println("callback data:", *data)
 }
@@ -48,4 +58,280 @@ func TestMongoInput_Start(t *testing.T) {
 	go plugin.Start(ch)
 	go monitorDump(ch, plugin, t)
 	time.Sleep(1000 * time.Second)
+}
+*/
+
+func TestMongoInput_GetUriExample(t *testing.T) {
+	c := new(MongoInput)
+	Convey("normal", t, func() {
+		uri, html := c.GetUriExample()
+		So(uri, ShouldNotEqual, "")
+		So(html, ShouldNotEqual, "")
+	})
+}
+
+func TestMongoInput_SetOption(t *testing.T) {
+	c := new(MongoInput)
+	patches := gomonkey.ApplyMethod(reflect.TypeOf(c), "GTID2OpLogPosition", func(c *MongoInput, GTID string) *primitive.Timestamp {
+		return nil
+	})
+	defer patches.Reset()
+	Convey("normal", t, func() {
+		c.SetOption(inputDriver.InputInfo{}, nil)
+	})
+}
+
+func TestMongoInput_setStatus(t *testing.T) {
+	c := new(MongoInput)
+	Convey("status close", t, func() {
+		c.setStatus(inputDriver.CLOSED)
+		So(c.err, ShouldNotBeNil)
+	})
+	Convey("PluginStatusChan is nil", t, func() {
+		c.setStatus(inputDriver.STARTING)
+	})
+	Convey("PluginStatusChan is not nil", t, func() {
+		ch := make(chan *inputDriver.PluginStatus, 2)
+		c.PluginStatusChan = ch
+		c.setStatus(inputDriver.STARTING)
+		var hadVal = false
+		select {
+		case <-ch:
+			hadVal = true
+			break
+		default:
+			break
+		}
+		So(hadVal, ShouldEqual, true)
+	})
+}
+
+func TestMongoInput_Start(t *testing.T) {
+	c := new(MongoInput)
+	patches := gomonkey.ApplyMethod(reflect.TypeOf(c), "Start0", func(c *MongoInput) error {
+		return nil
+	})
+	defer patches.Reset()
+	Convey("start", t, func() {
+		err := c.Start(make(chan *inputDriver.PluginStatus, 2))
+		So(err, ShouldBeNil)
+	})
+}
+
+func TestMongoInput_Start0(t *testing.T) {
+	c := new(MongoInput)
+	patches := gomonkey.ApplyMethod(reflect.TypeOf(c), "Start1", func(c *MongoInput) error {
+		return nil
+	})
+	defer patches.Reset()
+	Convey("time out", t, func() {
+		ctx, _ := context.WithTimeout(context.Background(), 4*time.Second)
+		go func() {
+			_ = c.Start0()
+			ctx.Done()
+		}()
+		select {
+		case _ = <-ctx.Done():
+			t.Log("success")
+		case _ = <-time.After(8 * time.Second):
+			t.Error("c.Start0 time out")
+		}
+	})
+
+	Convey("by stop", t, func() {
+		go func() {
+			_ = c.Start0()
+		}()
+		// 这里睡眠1秒，是为了防止协程里修改了c.ctx值，但是在主线程中没被更新，而导致异常
+		time.Sleep(1 * time.Second)
+		c.ctxCancleFun()
+		select {
+		case _ = <-c.ctx.Done():
+			t.Log("success")
+		case _ = <-time.After(8 * time.Second):
+			t.Error("c.Start0 time out")
+		}
+	})
+
+}
+
+func TestMongoInput_Start1(t *testing.T) {
+	c := new(MongoInput)
+
+	Convey("CreateMongoClient error", t, func() {
+		patches := gomonkey.ApplyFunc(CreateMongoClient, func(uri string, ctx context.Context) (*mongo.Client, error) {
+			return &mongo.Client{}, fmt.Errorf("mock error")
+		})
+		defer patches.Reset()
+		err := c.Start1()
+		So(err, ShouldNotBeNil)
+	})
+
+	Convey("normal", t, func() {
+		patches := gomonkey.ApplyMethod(reflect.TypeOf(c), "ConsumeMongoOpLog", func(c *MongoInput, ctx *gtm.OpCtx) {
+			return
+		})
+		patches.ApplyMethod(reflect.TypeOf(c), "GtmAfter", func(c *MongoInput, client *mongo.Client, options *gtm.Options) (primitive.Timestamp, error) {
+			return primitive.Timestamp{}, nil
+		})
+		patches.ApplyFunc(CreateMongoClient, func(uri string, ctx context.Context) (*mongo.Client, error) {
+			return &mongo.Client{}, nil
+		})
+		defer patches.Reset()
+		err := c.Start1()
+		So(err, ShouldBeNil)
+	})
+}
+
+func TestMongoInput_GtmAfter(t *testing.T) {
+	c := new(MongoInput)
+
+	Convey("return", t, func() {
+		c.currentPosition = &primitive.Timestamp{}
+		_, err := c.GtmAfter(&mongo.Client{}, &gtm.Options{})
+		So(err, ShouldBeNil)
+	})
+
+}
+
+func TestMongoInput_OpFitler(t *testing.T) {
+
+	Convey("Operation c false", t, func() {
+		c := new(MongoInput)
+		op := &gtm.Op{}
+		op.Operation = "c"
+
+		patches := gomonkey.ApplyMethod(reflect.TypeOf(op), "IsDropCollection", func(op *gtm.Op) (string, bool) {
+			return "", false
+		})
+		patches.ApplyMethod(reflect.TypeOf(op), "GetDatabase", func(op *gtm.Op) string {
+			return "database"
+		})
+		patches.ApplyMethod(reflect.TypeOf(op), "IsDropDatabase", func(op *gtm.Op) (string, bool) {
+			return "database", true
+		})
+		b := c.OpFitler(op)
+		So(b, ShouldEqual, true)
+	})
+
+	Convey("Operation insert true", t, func() {
+		c := new(MongoInput)
+		op := &gtm.Op{}
+		op.Operation = "i"
+
+		patches := gomonkey.ApplyMethod(reflect.TypeOf(op), "GetCollection", func(op *gtm.Op) string {
+			return "table"
+		})
+		patches.ApplyMethod(reflect.TypeOf(op), "GetDatabase", func(op *gtm.Op) string {
+			return "database"
+		})
+		b := c.OpFitler(op)
+		So(b, ShouldEqual, true)
+	})
+
+}
+
+func TestMongoInput_ConsumeMongoOpLog(t *testing.T) {
+
+	Convey("OpCtx done", t, func() {
+		c := new(MongoInput)
+		c.PluginStatusChan = make(chan *inputDriver.PluginStatus, 10)
+		c.ctx, c.ctxCancleFun = context.WithTimeout(context.Background(), 20*time.Second)
+		opCtx := &gtm.OpCtx{ErrC: make(chan error, 100)}
+		patches := gomonkey.ApplyMethod(reflect.TypeOf(c), "ToInputCallback", func(c *MongoInput, op *gtm.Op) {
+			return
+		})
+		defer patches.Reset()
+		opCtx.ErrC <- fmt.Errorf("mock error")
+		close(opCtx.ErrC)
+		c.ConsumeMongoOpLog(opCtx)
+	})
+
+	Convey("OpC msg", t, func() {
+		c := new(MongoInput)
+		c.PluginStatusChan = make(chan *inputDriver.PluginStatus, 10)
+		c.ctx, c.ctxCancleFun = context.WithTimeout(context.Background(), 20*time.Second)
+		opCtx := &gtm.OpCtx{ErrC: make(chan error, 100)}
+		patches := gomonkey.ApplyMethod(reflect.TypeOf(c), "ToInputCallback", func(c *MongoInput, op *gtm.Op) {
+			return
+		})
+		defer patches.Reset()
+		go func() {
+			opCtx.OpC <- &gtm.Op{}
+			c.ctxCancleFun()
+		}()
+		c.ConsumeMongoOpLog(opCtx)
+	})
+}
+
+func TestMongoInput_Stop(t *testing.T) {
+	c := new(MongoInput)
+	Convey("return", t, func() {
+		c.PluginStatusChan = make(chan *inputDriver.PluginStatus, 10)
+		c.ctx, c.ctxCancleFun = context.WithTimeout(context.Background(), 20*time.Second)
+		So(c.Stop(), ShouldBeNil)
+	})
+}
+
+func TestMongoInput_Close(t *testing.T) {
+	c := new(MongoInput)
+	Convey("return", t, func() {
+		c.PluginStatusChan = make(chan *inputDriver.PluginStatus, 10)
+		c.ctx, c.ctxCancleFun = context.WithTimeout(context.Background(), 20*time.Second)
+		So(c.Close(), ShouldBeNil)
+	})
+}
+
+func TestMongoInput_Kill(t *testing.T) {
+	c := new(MongoInput)
+	Convey("return", t, func() {
+		c.PluginStatusChan = make(chan *inputDriver.PluginStatus, 10)
+		c.ctx, c.ctxCancleFun = context.WithTimeout(context.Background(), 20*time.Second)
+		So(c.Kill(), ShouldBeNil)
+	})
+}
+
+func TestMongoInput_GetLastPosition(t *testing.T) {
+	c := &MongoInput{}
+	Convey("lastOp nil", t, func() {
+		So(c.GetLastPosition(), ShouldBeNil)
+	})
+
+	Convey("lastOp is not nil", t, func() {
+		patches := gomonkey.ApplyMethod(reflect.TypeOf(c), "OpLogPosition2GTID", func(c *MongoInput, p *primitive.Timestamp) string {
+			return ""
+		})
+		defer patches.Reset()
+		c.lastOp = &gtm.Op{}
+		So(c.GetLastPosition(), ShouldNotBeNil)
+	})
+}
+
+func TestMongoInput_SetCallback(t *testing.T) {
+	c := &MongoInput{}
+	Convey("callback nil", t, func() {
+		c.SetCallback(nil)
+	})
+	Convey("callback is function", t, func() {
+		var callback = func(data *outputDriver.PluginDataType) {
+			return
+		}
+		c.SetCallback(callback)
+	})
+}
+
+func TestMongoInput_SetEventID(t *testing.T) {
+	c := &MongoInput{}
+	Convey("set 0", t, func() {
+		c.SetEventID(0)
+	})
+}
+
+func TestMongoInput_getNextEventID(t *testing.T) {
+	c := &MongoInput{}
+	Convey("set 0", t, func() {
+		c.SetEventID(0)
+		id := c.getNextEventID()
+		So(id, ShouldEqual, 1)
+	})
 }
