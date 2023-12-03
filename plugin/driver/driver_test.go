7cb9edea5c5df282a124175a953e206b76e55f83
jc3wish <>
2023-12-03 21:12:28 +0800

    修复2.2.2 mysql,clickhouse之外的其他插件input没有配置tag标签导致数据转换失败的bug

--- plugin/driver/driver_test.go
+++ plugin/driver/driver_test.go
@@ -3,7 +3,9 @@ package driver
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/smartystreets/goconvey/convey"
 	"regexp"
+	"strconv"
 	"testing"
 	"time"
 )
@@ -31,28 +33,77 @@ func TestTransfeResult(t *testing.T) {
 		BinlogFileNum:  1,
 		BinlogPosition: 1000,
 		Pri:            Pri,
+		Gtid:           "gtidTest",
 	}
+	convey.Convey("normal", t, func() {
+		r1 := TransfeResult("{$json1[json]['0']['testkey']}", data, 0)
+		convey.So(r1, convey.ShouldEqual, "testVal")
 
-	r1 := TransfeResult("{$json1[json]['0']['testkey']}", data, 0)
-	if fmt.Sprint(r1) == "testVal" {
-		t.Log("success")
-	} else {
-		t.Fatal("r1:", fmt.Sprint(r1), " != ", "testVal")
-	}
+		b, _ := json.Marshal(data)
+		var data2 PluginDataType
+		err := json.Unmarshal(b, &data2)
+		if err != nil {
+			t.Fatal(err)
+		}
+		r10 := TransfeResult("{$json1[json]['0']['testkey']}", &data2, 0)
+		convey.So(r10, convey.ShouldEqual, "testVal")
 
-	b, _ := json.Marshal(data)
-	var data2 PluginDataType
-	err := json.Unmarshal(b, &data2)
-	if err != nil {
-		t.Fatal(err)
-	}
+		convey.So(TransfeResult("{$key1}", data, 0), convey.ShouldEqual, "key1")
+		convey.So(TransfeResult("{$SchemaName}", data, 0), convey.ShouldEqual, "bifrost_test")
+		convey.So(TransfeResult("{$TableName}", data, 0), convey.ShouldEqual, "bifrost_test_table")
+		convey.So(TransfeResult("{$EventType}", data, 0), convey.ShouldEqual, data.EventType)
+		convey.So(TransfeResult("{$BinlogTimestamp}", data, 0), convey.ShouldEqual, fmt.Sprint(data.Timestamp))
+		convey.So(TransfeResult("{$BinlogDateTime}", data, 0), convey.ShouldEqual, time.Unix(int64(data.Timestamp), 0).Format("2006-01-02 15:04:05"))
+		convey.So(TransfeResult("{$BinlogFileNum}", data, 0), convey.ShouldEqual, "1")
+		convey.So(TransfeResult("{$BinlogPosition}", data, 0), convey.ShouldEqual, "1000")
+		convey.So(TransfeResult("{$GTID}", data, 0), convey.ShouldEqual, data.Gtid)
+		convey.So(TransfeResult("{$BifrostNull}", data, 0), convey.ShouldBeNil)
 
-	r2 := TransfeResult("{$json1[json]['0']['testkey']}", &data2, 0)
-	if fmt.Sprint(r2) == "testVal" {
-		t.Log("success")
-	} else {
-		t.Fatal("r1:", fmt.Sprint(r2), " != ", "testVal")
-	}
+		nowTimeString := TransfeResult("{$Timestamp}", data, 0)
+		nowTimeInt64, err := strconv.ParseInt(fmt.Sprint(nowTimeString), 10, 64)
+		convey.So(err, convey.ShouldBeNil)
+		nowTime := time.Unix(nowTimeInt64, 0)
+		convey.So(nowTime.IsZero(), convey.ShouldEqual, false)
+
+		tags := "{$SchemaName}-{$TableName}-{$json1[json]['0']['testkey']}-{$BifrostNull}"
+
+		newVal := TransfeResult(tags, data, 0)
+		needNewVal := fmt.Sprintf("%s-%s-%s-", data.SchemaName, data.TableName, jsonData["json"][0]["testkey"])
+		convey.So(newVal, convey.ShouldEqual, needNewVal)
+
+	})
+
+	convey.Convey("data is nil", t, func() {
+		r1 := TransfeResult("{$json1[json]['0']['testkey']}", nil, 0)
+		convey.So(r1, convey.ShouldBeNil)
+	})
+
+	convey.Convey("noTagsReturnNil", t, func() {
+		r1 := TransfeResult("noTags", data, 0, true)
+		convey.So(r1, convey.ShouldBeNil)
+	})
+
+	convey.Convey("noTagsReturnNil == false", t, func() {
+		r1 := TransfeResult("noTags", data, 0)
+		convey.So(r1, convey.ShouldEqual, "noTags")
+	})
+
+	convey.Convey("tags not be found ,data is normal", t, func() {
+		r1 := TransfeResult("{$keyNotBeFound0}", data, 0)
+		convey.So(r1, convey.ShouldEqual, "{$keyNotBeFound0}")
+
+		r2 := TransfeResult("{$keyNotBeFound0}-{$keyNotBeFound1}", data, 0)
+		convey.So(r2, convey.ShouldEqual, "{$keyNotBeFound0}-{$keyNotBeFound1}")
+	})
+
+	convey.Convey("tags not be found and rows is nil", t, func() {
+		data.Rows = nil
+		r1 := TransfeResult("{$keyNotBeFound0}", data, 0)
+		convey.So(r1, convey.ShouldEqual, nil)
+
+		r2 := TransfeResult("{$keyNotBeFound0}-{$keyNotBeFound1}", data, 0)
+		convey.So(r2, convey.ShouldEqual, "<nil>-<nil>")
+	})
 }
 
 func TestReg(t *testing.T) {
